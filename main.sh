c3ce9d7ca2862772ceb6343d8044eccf34dba74e
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-21 21:36:42 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #！注意：此处下载的可能不是最新版cloudreve
 #！启动的话，直接run就行。。。
 #！初始管理员账号：admin@cloudreve.org；初始管理员密码在Console的info里
+#！登录后点击头像前往管理面板，更改参数设置中的站点信息为你的repl域名
 
 if [ ! -f "panindex" ];then
   curl -L https://github.com/cloudreve/Cloudreve/releases/download/3.5.3/cloudreve_3.5.3_linux_amd64.tar.gz -o cloudreve.tar.gz
