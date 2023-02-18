92237d69ed069958ccef681664ed228b223cd040
gityzon <109445070+gityzon@users.noreply.github.com>
2023-02-18 21:55:17 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,9 +4,9 @@
 #！登录后点击头像前往管理面板，更改参数设置中的站点信息为你的repl域名
 
 if [ ! -f "cloudreve" ];then
-  curl -L https://github.com/cloudreve/Cloudreve/releases/download/3.5.3/cloudreve_3.5.3_linux_amd64.tar.gz -o cloudreve.tar.gz
+  curl -L https://github.com/cloudreve/Cloudreve/releases/download/3.7.1/cloudreve_3.7.1_linux_amd64.tar.gz -o cloudreve.tar.gz
 tar -zxvf cloudreve.tar.gz
-rm -f cloudreve.tar.gz
+rm -f cloudreve.tar.gz && rm -f LICENSE
 fi
 chmod +x cloudreve
 ./cloudreve
